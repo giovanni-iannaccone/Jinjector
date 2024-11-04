78404fd1355dd345f6f526606c5a96587b850711
giovanni-iannaccone <iannacconegiovanni444@gmail.com>
2024-11-04 18:24:55 +0100

    Improve utils test

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,15 +1,17 @@
 package utils
 
 import (
+	"os"
 	"testing"
 )
 
 func TestFileWriteAndReading(t *testing.T) {
 	var testString string = "test"
+	var testFile string = "../../test/test.txt"
+	AppendToFile(testString, testFile)
+	buffer, _ := ReadFile(testFile)
 
-	AppendToFile(testString, "../../test/test.txt")
-	buffer, _ := ReadFile("../../test/test.txt")
-
+	os.WriteFile(testFile, []byte(""), 2)
 	if buffer != testString {
 		t.Fatalf("Read and write => Failed: %s", buffer)
 	}
