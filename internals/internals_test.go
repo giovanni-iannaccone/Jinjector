abf5234e871462822ea8d3e8a36ec8539709b35a
giovanni-iannaccone <iannacconegiovanni444@gmail.com>
2024-11-04 18:34:09 +0100

    Add tests for /internals functions

---
+++ internals/internals_test.go
@@ -0,0 +1,18 @@
+package internals
+
+import "testing"
+
+func TestFindModuleName(t *testing.T) {
+	var path string = "test/module/name/fake_module_name"
+	var moduleName = findModuleName(path)
+	if moduleName != "fake_module_name" {
+		t.Fatalf("Finding module's name => Failed: %s", moduleName)
+	}
+}
+
+func TestFindMainFileDirectory(t *testing.T) {
+	mainDirectory, _ := findMainFileDirectory("../test", "test_module")
+	if mainDirectory != "test/directory/main.php" {
+		t.Fatalf("Finding module's main file => Failed: %s", mainDirectory)
+	}
+}
