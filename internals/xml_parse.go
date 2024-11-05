b8769f321c14001d8a0d7cb3e6c6c28c43c0294f
giovanni-iannaccone <iannacconegiovanni444@gmail.com>
2024-11-05 16:05:34 +0100

    Fix xml parse function

--- internals/xml_parse.go
+++ internals/xml_parse.go
@@ -2,23 +2,42 @@ package internals
 
 import (
 	"encoding/xml"
+	"errors"
 	"os"
 )
 
-type Filename struct {
-    Module string `xml:"module,attr"`
-    Name   string `xml:"files>filename"`
+type File struct {
+	Module string `xml:"module,attr"`
+	Dir    string `xml:",chardata"`
+}
+
+type Files struct {
+	Filenames []File `xml:"filename"`
+}
+
+type Extension struct {
+	Files Files `xml:"files"`
 }
 
 func findMainFileDirectory(path string, moduleName string) (string, error) {
-	var xmlFileName string = path + "/" + moduleName + ".xml"
-	var file Filename
+	xmlFileName := path + "/" + moduleName + ".xml"
+	var extension Extension
 
 	byteValue, err := os.ReadFile(xmlFileName)
 	if err != nil {
 		return "", err
 	}
 
-	xml.Unmarshal(byteValue, &file)
-	return file.Name, nil
+	err = xml.Unmarshal(byteValue, &extension)
+	if err != nil {
+		return "", err
+	}
+
+	for _, file := range extension.Files.Filenames {
+		if file.Module == moduleName {
+			return file.Dir, nil
+		}
+	}
+
+	return "", errors.New("can't find main file")
 }
